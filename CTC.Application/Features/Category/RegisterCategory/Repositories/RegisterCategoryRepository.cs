d5b4efce5d0044f7ea380bb8efe47f699b978066
Tomas Campos <tomasantoniocampos@gmail.com>
2023-01-13 10:28:11 -0300

    Ajustando nome de classe

--- CTC.Application/Features/Category/RegisterCategory/Repositories/RegisterCategoryRepository.cs
+++ CTC.Application/Features/Category/RegisterCategory/Repositories/RegisterCategoryRepository.cs
@@ -5,21 +5,21 @@ namespace CTC.Application.Features.Category.RegisterCategory.Repositories
 {
     internal sealed class RegisterCategoryRepository : IRegisterCategoryRepository
     {
-        private readonly ISqlService _mySqlDataBaseConnector;
+        private readonly ISqlService _sqlService;
 
         public RegisterCategoryRepository(ISqlService mySqlDataBaseConnector)
         {
-            _mySqlDataBaseConnector = mySqlDataBaseConnector;
+            _sqlService = mySqlDataBaseConnector;
         }
 
         public async Task<int> CountCategoryByName(string categoryName)
         {
-            return await _mySqlDataBaseConnector.CountAsync(RegisterCategorySqlScripts.COUNT_CATEGORY_BY_NAME_SQL, new { category_name = categoryName });
+            return await _sqlService.CountAsync(RegisterCategorySqlScripts.COUNT_CATEGORY_BY_NAME_SQL, new { category_name = categoryName });
         }
 
         public async Task InsertCategory(CategoryModel model)
         {
-            _ = await _mySqlDataBaseConnector.ExecuteAsync(RegisterCategorySqlScripts.INSERT_CATEGORY_SQL, new { category_id = model.CategoryId, category_name = model.CategoryName});
+            _ = await _sqlService.ExecuteAsync(RegisterCategorySqlScripts.INSERT_CATEGORY_SQL, new { category_id = model.CategoryId, category_name = model.CategoryName});
         }
     }
 }
