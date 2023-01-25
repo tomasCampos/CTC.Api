d02e4c30c68167cee9a2ff67bab367d0840caadd
Tomas Campos <tomasantoniocampos@gmail.com>
2023-01-24 23:03:56 -0300

    refactor

--- CTC.Application/Features/User/UseCases/RegisterUser/UseCase/RegisterUserUseCase.cs
+++ CTC.Application/Features/User/UseCases/RegisterUser/UseCase/RegisterUserUseCase.cs
@@ -49,7 +49,6 @@ namespace CTC.Application.Features.User.UseCases.RegisterUser.UseCase
 
             var encryptedPassword = AES.Encrypt(input.UserPassword!, AESKey);
             var user = new UserModel(input.UserFirstName!, input.UserEmail!, input.UserPhone!, input.UserDocument!, input.UserLastName!, (int)input.UserPermission!, encryptedPassword);
-
             var wasUserInsertedInDataBaseWithSuccess = await _repository.InsertUser(user);
             if (!wasUserInsertedInDataBaseWithSuccess)
             {
