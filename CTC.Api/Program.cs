a9c940624e070a824fac1344e0b05386a449c279
Tomas Campos <tomasantoniocampos@gmail.com>
2023-03-09 09:49:13 -0300

    configurando cors policy

--- CTC.Api/Program.cs
+++ CTC.Api/Program.cs
@@ -7,6 +7,18 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
+const string allowedOrigins = "CtcApiAllowedOrigins";
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy(allowedOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173", "https://ctcconstrucoes.vercel.app")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        });
+});
+
 // For running in Railway
 var portVar = Environment.GetEnvironmentVariable("PORT");
 if (portVar is { Length: > 0 } && int.TryParse(portVar, out int port))
@@ -25,6 +37,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 
 var app = builder.Build();
+
+//Add cors policy
+app.UseCors(allowedOrigins);
 
 // Configure the HTTP request pipeline.
 app.UseSwagger();
