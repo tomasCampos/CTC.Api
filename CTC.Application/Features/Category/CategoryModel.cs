2bebd94a10ac589b20b91270c27d8882d86c06f8
Tomas Campos <tomasantoniocampos@gmail.com>
2023-03-10 14:33:07 -0300

    refactor

--- CTC.Application/Features/Category/CategoryModel.cs
+++ CTC.Application/Features/Category/CategoryModel.cs
@@ -6,20 +6,12 @@ namespace CTC.Application.Features.Category
     {
         public CategoryModel(in string categoryName)
         {
-            if(string.IsNullOrWhiteSpace(categoryName))
-                throw new ArgumentNullException(nameof(categoryName));
-
             Name = categoryName;
             Id = Guid.NewGuid().ToString();
         }
 
         public CategoryModel(in string categoryName, in string categoryId)
         {
-            if (string.IsNullOrWhiteSpace(categoryName))
-                throw new ArgumentNullException(nameof(categoryName));
-            if (string.IsNullOrWhiteSpace(categoryId))
-                throw new ArgumentNullException(nameof(categoryId));
-
             Name = categoryName;
             Id = categoryId;
         }
