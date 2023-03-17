e04c68b8efef09e6f0b775a369482c29ce742b30
Ana Paula Buchholz <anapbuchholz@gmail.com>
2023-03-16 21:16:03 -0300

    [feature] Implementando listagem de fornecedores

--- CTC.Api/Controllers/Supplier/SupplierController.cs
+++ CTC.Api/Controllers/Supplier/SupplierController.cs
@@ -1,8 +1,8 @@
 ﻿using CTC.Api.Controllers.Supplier.Contracts;
-using CTC.Api.Controllers.User.Contracts;
 using CTC.Api.Shared;
+using CTC.Application.Features.Supplier.UseCases.ListSuppliers.UseCase;
 using CTC.Application.Features.Supplier.UseCases.RegisterSupplier.UseCase;
-using CTC.Application.Features.User.UseCases.RegisterUser.UseCase;
+using CTC.Application.Shared.Request;
 using CTC.Application.Shared.UseCase;
 using CTC.Application.Shared.UseCase.IO;
 using Microsoft.AspNetCore.Authorization;
@@ -16,10 +16,13 @@ namespace CTC.Api.Controllers.Supplier
     public sealed class SupplierController : BaseController
     {
         private readonly IUseCase<RegisterSupplierInput, Output> _registerSupplierUseCase;
+        private readonly IUseCase<ListSuppliersUseCaseInput, Output> _listSuppliersUseCase;
 
-        public SupplierController(IUseCase<RegisterSupplierInput, Output> registerSupplierUseCase)
+        public SupplierController(IUseCase<RegisterSupplierInput, Output> registerSupplierUseCase,
+                                  IUseCase<ListSuppliersUseCaseInput, Output> listSuppliersUseCase)
         {
             _registerSupplierUseCase = registerSupplierUseCase;
+            _listSuppliersUseCase = listSuppliersUseCase;
         }
 
         [Authorize]
@@ -35,6 +38,19 @@ namespace CTC.Api.Controllers.Supplier
 
             var output = await _registerSupplierUseCase.Execute(input);
             return GetHttpResponse(output, "/user");
+        }
+
+        [Authorize]
+        [HttpGet()]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> ListSuppliers([FromQuery] int pageNumber, [FromQuery] int pageSize, [FromQuery] string? queryParam)
+        {
+            var request = QueryRequest.Create(pageNumber, pageSize, queryParam);
+            var input = new ListSuppliersUseCaseInput(request);
+            var output = await _listSuppliersUseCase.Execute(input);
+            return GetHttpResponse(output);
         }
     }
 }
---
+++ CTC.Application/Features/Supplier/UseCases/ListSuppliers/UseCase/ListSuppliersUseCase.cs
@@ -0,0 +1,24 @@
+﻿using CTC.Application.Features.Supplier.UseCases.ListSuppliers.Data;
+using CTC.Application.Shared.Authorization;
+using CTC.Application.Shared.UseCase;
+using CTC.Application.Shared.UseCase.IO;
+using System.Threading.Tasks;
+
+namespace CTC.Application.Features.Supplier.UseCases.ListSuppliers.UseCase
+{
+    internal sealed class ListSuppliersUseCase : IUseCase<ListSuppliersUseCaseInput, Output>
+    {
+        private readonly IListSuppliersRepository _suppliersRepository;
+
+        public ListSuppliersUseCase(IUseCaseAuthorizationService useCaseAuthorizationService, IListSuppliersRepository suppliersRepository)
+        {
+            _suppliersRepository = suppliersRepository;
+        }
+        
+        public async Task<Output> Execute(ListSuppliersUseCaseInput input)
+        {
+            var result = await _suppliersRepository.ListSuppliers(input.Request);
+            return Output.CreateOkResult(result);
+        }
+    }
+}
