7c6a5ba08c011ac0d619d9272f608ef88a547825
Tomas Campos <tomasantoniocampos@gmail.com>
2023-03-20 22:35:22 -0300

    Authorize User

---
+++ CTC.Application/Features/User/Services/Firebase/FirebaseService.cs
@@ -0,0 +1,35 @@
+﻿using Firebase.Auth;
+using System.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace CTC.Application.Features.User.Services.Firebase
+{
+    internal sealed class FirebaseService : IFirebaseService
+    {
+        private readonly FirebaseAuthProvider _authProvider;
+
+        private const string FireBaseApiKeyEnvironmentVariableName = "FIRE_BASE_API_KEY";
+
+        public FirebaseService()
+        {
+            var fireBaseApiKey = Environment.GetEnvironmentVariable(FireBaseApiKeyEnvironmentVariableName)
+                ?? throw new ConfigurationErrorsException($"Missing environment variable named {FireBaseApiKeyEnvironmentVariableName}");
+
+            _authProvider = new FirebaseAuthProvider(new FirebaseConfig(fireBaseApiKey));
+        }
+
+        public async Task<(bool sucess, string? token)> LoginInFireBase(string userEmail, string userPassword)
+        {
+            try
+            {
+                FirebaseAuthLink firebaseAuthLink = await _authProvider.SignInWithEmailAndPasswordAsync(userEmail, userPassword);
+                return (true, firebaseAuthLink.FirebaseToken);
+            }
+            catch
+            {
+                return (false, null);
+            }
+        }
+    }
+}
---
+++ CTC.Application/Features/User/Services/Firebase/IFirebaseService.cs
@@ -0,0 +1,10 @@
+﻿using CTC.Application.Features.User.UseCases.AuthorizeUser.UseCase;
+using System.Threading.Tasks;
+
+namespace CTC.Application.Features.User.Services.Firebase
+{
+    internal interface IFirebaseService
+    {
+        Task<(bool sucess, string? token)> LoginInFireBase(string userEmail, string userPassword);
+    }
+}
