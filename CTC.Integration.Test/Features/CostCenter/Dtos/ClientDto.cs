abbdaf32c5d8fac37b9aa3cd48063e83471f4090
Tomas Campos <tomasantoniocampos@gmail.com>
2023-06-05 21:54:58 -0300

    Cria testes de centro de custos

---
+++ CTC.Integration.Test/Features/CostCenter/Dtos/ClientDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CTC.Integration.Test.Features.CostCenter.Dtos
+{
+    internal sealed class ClientDto
+    {
+        public string? Id { get; set; }
+        public string? ClientName { get; set; }
+        public string? ClientDocument { get; set; }
+    }
+}
